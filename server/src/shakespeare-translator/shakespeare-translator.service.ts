08ca1c5aa5e0af852d37ba586805b5d62cc77582
Brendan Short <brendan@reposit.co.uk>
2020-08-02 18:31:21 +0000

    Move server into sever folder

--- server/src/shakespeare-translator/shakespeare-translator.service.ts
+++ server/src/shakespeare-translator/shakespeare-translator.service.ts
@@ -6,10 +6,20 @@ import { appendToFile } from '../../test/utils/file-utils';
 
 @Injectable()
 export class ShakespeareTranslatorService {
+  private headers: { [key: string]: string };
+
   constructor(
     private httpService: HttpService,
     private configService: ConfigService,
-  ) {}
+  ) {
+    const apiKey = this.configService.get('FUN_TRANSLATIONS_API_KEY');
+
+    if (apiKey) {
+      this.headers = {
+        'X-Funtranslations-Api-Secret': apiKey,
+      };
+    }
+  }
 
   async translate(text: string): Promise<TranslationResponseDTO> {
     const url = `${this.configService.get(
@@ -19,11 +29,7 @@ export class ShakespeareTranslatorService {
     try {
       const response: AxiosResponse<TranslationResponseDTO> = await this.httpService
         .post<TranslationResponseDTO>(url, null, {
-          headers: {
-            'X-Funtranslations-Api-Secret': this.configService.get(
-              'FUN_TRANSLATIONS_API_KEY',
-            ),
-          },
+          headers: this.headers,
         })
         .toPromise();
 
